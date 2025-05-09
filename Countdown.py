8de1b4db7f71fc2af2afc76e627e48b3204cbd6c
Abdulraheem <eremirogers@gmail.com>
2025-05-09 07:24:35 +0100

    Add Countdown script

---
+++ Countdown.py
@@ -0,0 +1,13 @@
+
+
+n = int(input("Enter a positive integer: "))
+
+while n < 1:
+    print("Please enter a number greater than 0.")
+    n = int(input("Enter a positive integer: "))
+
+while n >= 1:
+    print(n)
+    n = n - 1
+
+print("Blast Off!")
